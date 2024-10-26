46013b40138b713cef259b12eae2475330c97238
Serhat Yegin <yeginserhat938@gmail.com>
2024-10-26 16:35:55 +0300

    Initial Commit

---
+++ MessengerSwiftUI/MessengerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerSwiftUIApp.swift
+//  MessengerSwiftUI
+//
+//  Created by serhat on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
